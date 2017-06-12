1002d667be5799eb2ff39fd5d6523225694830b7
Cristian Morataya <cris.morataya@gmail.com>
2017-06-12 00:49:49 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Square {
+	private:
+		int side_;
+	
+	public:
+		Square (int side_length) : side_ (side_length) { };
+		int getArea () const { return side_*side_; };
+};
+
+int main () {
+	Square square(5);
+	cout << "Area: " << square.getArea() << endl;
+	return 0;
+}
